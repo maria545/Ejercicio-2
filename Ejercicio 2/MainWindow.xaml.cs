f978ba042da183f6438efcf6fcd0652c608d2351
AlumnoT1 <maria.cabrera@iesdoctorbalmis.com>
2020-10-07 11:33:05 +0200

    He hecho un cambio

---
+++ Ejercicio 2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Ejercicio_2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { 
+            
+            InitializeComponent();
+        }
+
+        private void CalcularBoton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                switch (operadorTextBox.Text)
+                {
+                    case "+":
+                        resultadoTextBox.Text = int.Parse(operando1TextBox.Text) + int.Parse(operando2TextBox.Text) + "";
+                        break;
+                    case "-":
+                        resultadoTextBox.Text = int.Parse(operando1TextBox.Text) - int.Parse(operando2TextBox.Text) + "";
+                        break;
+                    case "*":
+                        resultadoTextBox.Text = int.Parse(operando1TextBox.Text) * int.Parse(operando2TextBox.Text) + "";
+                        break;
+                    case "/":
+                        resultadoTextBox.Text = int.Parse(operando1TextBox.Text) / int.Parse(operando2TextBox.Text) + "";
+                        break;
+                }
+            }catch(Exception)
+            {
+                MessageBox.Show("Se ha producido un error. Revise los comandos");
+            }
+            
+        }
+
+        
+        private void limpiarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            operando1TextBox.Text = "";
+            operando2TextBox.Text = "";
+            operadorTextBox.Text = "";
+            resultadoTextBox.Text = "";
+
+        }
+
+        private void operadorTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (operadorTextBox.Text == "+" || operadorTextBox.Text == "-" || operadorTextBox.Text == "*" || operadorTextBox.Text == "/")
+            {
+                calcularBoton.IsEnabled = true;
+            }
+            else 
+            {
+                calcularBoton.IsEnabled = false;
+            }
+        }
+    }
+}
